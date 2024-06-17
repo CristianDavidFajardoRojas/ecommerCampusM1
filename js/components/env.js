b6274c975a70a8223547dab0d872b0c4d27811d9
CritianDavidFajardoRojas <cfajardorojas98@gmail.com>
2024-06-17 08:37:16 -0500

    feat: :sparkles: Carga Automatica.
    Nueva funcionalidad de carga de productos al iniciar la pagina.

--- js/components/env.js
+++ js/components/env.js
@@ -1,4 +1,4 @@
 export const headers = {
-    'x-rapidapi-key': 'd866f1f41dmsh154466966fcd691p14145cjsna46851e0fa3e',
-	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
+    'x-rapidapi-key': 'eb13c9a8eamsh512e69514c463ddp1cdc5ajsn3aa1e85dfbf1',
+    'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 };
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { menuListCategoryIndex } from "./components/menu.js";
 import { galleryIndex } from "./components/gallery.js";
 import { getAllProductName, getAllCategory } from "./module/app.js";
+import { getAllClothesUnder10 } from "./module/app.js";
 
 let input__search = document.querySelector("#input__search");
 let main__article = document.querySelector(".main__article");
@@ -9,12 +10,21 @@ let nav__ul = document.querySelector(".nav__ul");
 addEventListener("DOMContentLoaded", async e=>{
     if(!localStorage.getItem("getAllCategory")) localStorage.setItem("getAllCategory", JSON.stringify(await getAllCategory()));
     nav__ul.innerHTML = await menuListCategoryIndex(JSON.parse(localStorage.getItem("getAllCategory")));
+    let params = new URLSearchParams(location.search);
+    let idCategory = params.get('id');
+    if(idCategory== undefined){
+        let res = await getAllClothesUnder10();
+        main__article.innerHTML = galleryIndex(res, "Fashion");
+    }else{
+        let res = await getAllClothesUnder10();
+        main__article.innerHTML = galleryIndex(res, idCategory);
+    }
 })
 
 input__search.addEventListener("change", async e => {
-    let params = new URLSearchParams(location.search)
-    let data = { search : e.target.value, id: params.get('id')}
+    let params = new URLSearchParams(location.search);
+    let data = { search : e.target.value, id: params.get('id')};
     input__search.value = null;
-    let res = await getAllProductName(data)
+    let res = await getAllProductName(data);
     main__article.innerHTML = galleryIndex(res, params.get('id'));
 });
